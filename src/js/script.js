2390f8918bd497c29d6f851fd37ed207cd185e53
yuki <meipapa0415@gmail.com>
2023-08-16 09:34:05 +0900

    ２回目コミット

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,21 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+    // ------------ドロワー --------------
 
+jQuery('.drawer-btns').on('click',function(){
+    jQuery('.drawer-btns').toggleClass('is-active');
+    jQuery('.drawer-btns span').toggleClass('is-active');
+    jQuery('.drawer-content').toggleClass('is-active');
+    jQuery('.drawer-background').toggleClass('is-active');
+    jQuery('body').toggleClass('scroll-lock');
+  });
+  
+  jQuery('.drawer-item a').on('click',function(){
+    
+  jQuery('.drawer-btns , .drawer-btns span').removeClass('is-active');
+  jQuery('.drawer-content').removeClass('is-active');
+  jQuery('.drawer-background').removeClass('is-active');
+  jQuery('body').removeClass('scroll-lock');
+  });
 });
