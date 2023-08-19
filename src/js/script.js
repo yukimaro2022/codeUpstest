69f4acc27e7acffe584b3a874a95e4ef0095722c
yuki <meipapa0415@gmail.com>
2023-08-19 11:17:23 +0900

    ６回目

--- src/js/script.js
+++ src/js/script.js
@@ -38,11 +38,18 @@ jQuery(function(){
     direction: 'horizontal',
     loop: true,
     spaceBetween: 24,
-    slidesPerView: 1.2, 
+
     breakpoints: {
       // when window width is >= 320px
       768: {
         slidesPerView:3.2,
+      },
+      
+      600: {
+        slidesPerView:2,
+      },
+      375: {
+        slidesPerView:1.2,
       },
     },
     navigation: {
@@ -52,3 +59,50 @@ jQuery(function(){
     spaceBetween: 24, // スライド間の隙間を20ピクセルに設定
   });
 });
+  
+  // #から始まるURLがクリックされた時
+jQuery('a[href^="#"]').click(function() {
+  
+  // .headerクラスがついた要素の高さを取得
+  let header = jQuery("header").innerHeight();
+  //スクロールスピードを設定する
+  let speed = 300;
+  
+  //押した要素のhref属性を取得する
+  let id = jQuery(this).attr("href");
+  
+  //＃の値がidならばhtmlを設定　id属性ではなければidを設定する
+  let target = jQuery("#" == id ? "html" : id);
+  // トップからの距離からヘッダー分の高さを引く
+  
+  let position = jQuery(target).offset().top - header;
+  
+  // その分だけ移動すればヘッダーと被りません
+  jQuery("html, body").animate(
+    {
+      scrollTop: position
+    },
+    speed
+  );
+  return false;
+});
+
+
+  // let js__fade = document.querySelector('.card__article');
+  // console.log(js__fade)
+  // gsap.to(js__fade, {
+  //   opacity: 1, // 例えば opacity をアニメーションする例
+  //   duration: 1, // アニメーションの時間
+  //   scrollTrigger: {
+  //     trigger: js__fade,
+  //     start: 'center top', // 画面内に表示されたときにアニメーションをトリガーする位置
+  //     markers: true // マーカーを表示するかどうか（デバッグ用）
+  //   },
+  // });
+  
+    jQuery('.js-fadeIn').on('inview', function(event, isInView) {
+      if (isInView) {
+        jQuery(this).addClass('is-inview');
+      }
+    });
+  
